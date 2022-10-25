35f5031c5a10d965b09f0f457d6b8cdf30e0dfb9
hidekishibashi <dev.hidekishibashi@gmail.com>
2022-10-26 00:39:52 +0900

    add: about page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,10 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe 'StaticPages' do
+  let(:common_title) { 'Ruby on Rails Tutorial Sample App' }
+
   describe 'GET /home' do
     it 'returns http success' do
       get '/static_pages/home'
       expect(response).to have_http_status(:success)
+      assert_select 'title', "Home | #{common_title}"
     end
   end
 
@@ -14,6 +17,15 @@ RSpec.describe 'StaticPages' do
     it 'returns http success' do
       get '/static_pages/help'
       expect(response).to have_http_status(:success)
+      assert_select 'title', "Help | #{common_title}"
+    end
+  end
+
+  describe 'GET /about' do
+    it 'returns http success' do
+      get '/static_pages/about'
+      expect(response).to have_http_status(:success)
+      assert_select 'title', "About | #{common_title}"
     end
   end
 end
